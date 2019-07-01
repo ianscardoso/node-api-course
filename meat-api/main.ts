58258181eb2e00c04e5e832fb4a2983486d11326
Warrian <ianscardoso@hotmail.com>
2019-07-01 20:42:22 -0300

    first restify example

--- meat-api/main.ts
+++ meat-api/main.ts
@@ -5,11 +5,42 @@ const server = restify.createServer({
     version: '1.0.0'
 })
 
+server.use(restify.plugins.queryParser())
+
 server.get('/hello', (req, res, next) => {
     res.json({ message: 'hello' });
     return next();
 })
 
+server.get('/info',
+    [(req, res, next) => { // it is possible to pass an array of callbacks
+        if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
+            // res.status(400);
+            // res.json({ message: 'Please, update your browser' });
+
+            // return next(false); // false skips the callback below
+
+            let error: any = new Error();
+            error.statusCode = 400;
+            error.message = 'Please, update your browser';
+
+            return next(error);
+        }
+
+        return next()
+    },
+    (req, res, next) => {
+        res.json({
+            browser: req.userAgent(),
+            method: req.method,
+            url: req.href(),
+            path: req.path(),
+            query: req.query
+        })
+
+        return next();
+    }])
+
 server.listen(3000, () => {
-    console.log('API is running on http://localhost:3000'); 
+    console.log('API is running on http://localhost:3000');
 })
