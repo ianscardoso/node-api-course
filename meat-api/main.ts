b345d77e818ba4ab581d13afccbbc5e482238c77
Warrian <ianscardoso@hotmail.com>
2019-07-20 11:27:41 -0300

    implementing first API resource and adding .gitignore

--- meat-api/main.ts
+++ meat-api/main.ts
@@ -1,46 +1,11 @@
-import * as restify from 'restify'
+import { Server } from './server/server'
+import { usersRouter } from './users/users.router'
 
-const server = restify.createServer({
-    name: 'meat-api',
-    version: '1.0.0'
-})
-
-server.use(restify.plugins.queryParser())
-
-server.get('/hello', (req, res, next) => {
-    res.json({ message: 'hello' });
-    return next();
-})
-
-server.get('/info',
-    [(req, res, next) => { // it is possible to pass an array of callbacks
-        if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
-            // res.status(400);
-            // res.json({ message: 'Please, update your browser' });
-
-            // return next(false); // false skips the callback below
-
-            let error: any = new Error();
-            error.statusCode = 400;
-            error.message = 'Please, update your browser';
-
-            return next(error);
-        }
-
-        return next()
-    },
-    (req, res, next) => {
-        res.json({
-            browser: req.userAgent(),
-            method: req.method,
-            url: req.href(),
-            path: req.path(),
-            query: req.query
-        })
-
-        return next();
-    }])
-
-server.listen(3000, () => {
-    console.log('API is running on http://localhost:3000');
+const server = new Server()
+server.bootstrap([usersRouter]).then(server => {
+    console.log('Server is listening on:', server.application.address())
+}).catch(error => {
+    console.log('Server failed to start')
+    console.log(error)
+    process.exit(1)
 })
