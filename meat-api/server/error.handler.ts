d36b4c8c50d5c6bf591c2bf7997d1f060277d4e5
Warrian <ianscardoso@hotmail.com>
2019-07-28 12:31:27 -0300

    implementing some built in validators from mongoose

--- meat-api/server/error.handler.ts
+++ meat-api/server/error.handler.ts
@@ -1,6 +1,8 @@
 import * as restify from 'restify'
 
 export const handleError = (req: restify.Request, res: restify.Response, err, done) => {
+    console.log(err)
+
     err.toJSON = () => {
         return {
             message: err.message
@@ -9,12 +11,21 @@ export const handleError = (req: restify.Request, res: restify.Response, err, do
 
     switch (err.name) {
         case 'MongoError':
-            if (err.code === 11000) { // 11000 = duplicate key error
+            if (err.code === 11000) {  // 11000 = duplicate key error
                 err.statusCode = 400
             }
             break
         case 'ValidationError':
             err.statusCode = 400
+            const messages: any[] = []
+
+            for (let e in err.errors) {
+                messages.push({ message: err.errors[e].message })
+            }
+            
+            err.toJSON = () => ({
+                errors: messages
+            })
             break
     }
 
