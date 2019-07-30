f3f0929509caf930e90c169fd4d17a848d523527
Warrian <ianscardoso@hotmail.com>
2019-07-29 22:49:45 -0300

    refactoring router and validating MongoDB's ObjectId

--- meat-api/users/users.router.ts
+++ meat-api/users/users.router.ts
@@ -1,81 +1,23 @@
-import { Router } from '../common/router'
 import * as restify from 'restify'
 import { User } from './users.model'
 import { NotFoundError } from 'restify-errors'
+import { ModelRouter } from '../common/model-router';
 
-class UsersRouter extends Router {
+class UsersRouter extends ModelRouter<User> {
     constructor() {
-        super()
+        super(User)
         this.on('beforeRender', document => {
             document.password = undefined //or delete document.password
         })
     }
 
     applyRoutes(application: restify.Server) {
-        application.get('/users', (req, res, next) => {
-            User.find()
-                .then(this.render(res, next))
-                .catch(next)
-        })
-
-        application.get('/users/:id', (req, res, next) => {
-            User.findById(req.params.id)
-                .then(this.render(res, next))
-                .catch(next)
-        })
-
-        application.post('/users', (req, res, next) => {
-            let user = new User(req.body)
-            user.save()
-                .then(this.render(res, next))
-                .catch(next)
-        })
-
-        application.put('/users/:id', (req, res, next) => {
-            // in theory, PUT replaces an object, but the mongoose update() would partially update the document, hence this option to overwrite it
-            const options = {
-                runValidators: true,
-                overwrite: true
-            }
-
-            User.update({ _id: req.params.id }, req.body, options).exec()
-                .then(result => {
-                    if (result.n) {
-                        return User.findById(req.params.id).exec()
-                    } else {
-                        throw new NotFoundError('Document not found')
-                    }
-                })
-                .then(this.render(res, next))
-                .catch(next)
-        })
-
-        application.patch('/users/:id', (req, res, next) => {
-            // by default findByIdAndUpdate() returns the old document (before update)
-            // this option makes the findByIdAndUpdate() return the new document (with the updates apllied)
-            const options = {
-                runValidators: true,
-                new: true
-            }
-
-            User.findByIdAndUpdate(req.params.id, req.body, options)
-                .then(this.render(res, next))
-                .catch(next)
-        })
-
-        application.del('/users/:id', (req, res, next) => {
-            User.remove({ _id: req.params.id }).exec()
-                .then((cmdResult: any) => {
-                    if (cmdResult.result.n) {
-                        res.send(204)
-                    } else {
-                        throw new NotFoundError('Document not found')
-                    }
-
-                    return next()
-                })
-                .catch(next)
-        })
+        application.get('/users', this.findAll)
+        application.get('/users/:id', [this.validateId, this.findById])
+        application.post('/users', this.save)
+        application.put('/users/:id', [this.validateId, this.replace])
+        application.patch('/users/:id', [this.validateId, this.update])
+        application.del('/users/:id', [this.validateId, this.delete])
     }
 }
 
